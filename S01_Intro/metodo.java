ed6331a9a157f28beea2e61b68eb7fa6df676dcc
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-24 19:24:13 -0500

    Seccion 01

---
+++ S01_Intro/metodo.java
@@ -0,0 +1,8 @@
+package S01_Intro;
+
+public class metodo {
+    public static void main(String[] args) {
+        // Imprimir "Hola Mundo" en la consola
+        System.out.println("Hola Mundo");
+    }
+}
