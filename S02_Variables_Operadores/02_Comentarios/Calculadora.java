4d644412fe6a4a277e6a34113a36d3a175cd8b71
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-25 23:55:31 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/02_Comentarios/Calculadora.java
@@ -0,0 +1,31 @@
+/**
+ * Clase Calculadora que implementa operaciones aritméticas básicas.
+ * <p>
+ * Esta clase permite sumar, restar, multiplicar y dividir dos números.
+ * También incluye una función para calcular el módulo.
+ * </p>
+ */
+public class Calculadora {
+
+    /**
+     * Suma dos números.
+     * @param a el primer número a sumar
+     * @param b el segundo número a sumar
+     * @return la suma de a y b
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Resta dos números.
+     * @param a el primer número
+     * @param b el número a restar de a
+     * @return la diferencia entre a y b
+     */
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    // ... (más métodos)
+}
