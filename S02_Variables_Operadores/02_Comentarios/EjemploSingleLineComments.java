0e5394f07daa2ed10c8568628e358a52e58e7e3c
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-25 23:49:33 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/02_Comentarios/EjemploSingleLineComments.java
@@ -0,0 +1,14 @@
+public class EjemploSingleLineComments {
+
+    public static void main(String[] args) {
+        // Declaración de variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Cálculo de la suma
+        int suma = num1 + num2;
+
+        // Impresión del resultado en la consola
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+    }
+}
