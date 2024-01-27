94ec1fcd732f52be443c747d297a67942100028b
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-26 23:53:53 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/09_Salida_Datos/EjemploPrint.java
@@ -0,0 +1,12 @@
+public class EjemploPrint {
+    public static void main(String [] args){
+        int[] numeros = {1,2,3,4,5};
+
+        for (int num : numeros){
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n");
+    }
+    
+}
