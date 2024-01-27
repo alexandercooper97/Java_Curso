93d29c387c747d302dd88fd86af9f01e55feb950
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-26 21:49:34 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/05_Operadores/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String [] args){
+        int a = 50;
+        int b = 6;
+        int c = 5;
+
+        System.out.println(a % b);
+        System.out.println( (a - b * c) / 4);
+
+        a = 10;
+        a %= 5;
+        System.out.println(a);
+    }
+    
+}
