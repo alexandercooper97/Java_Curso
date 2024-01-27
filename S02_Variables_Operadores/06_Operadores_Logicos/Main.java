e77c13330e3d6c43fe01152132127c27c26a089b
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-26 21:54:44 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/06_Operadores_Logicos/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String [] args){
+        int a = 3;
+        int b = 2;
+
+        System.out.println(a <= b);
+        System.out.println(! false);
+        System.out.println(false || false);
+        System.out.println(a == b || !(a  > b));
+    }
+    
+}
