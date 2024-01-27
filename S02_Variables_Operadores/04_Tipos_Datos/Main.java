61071cccd7dc64ff0ab6441e483aa8dd041ff0bb
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-01-26 21:44:26 -0500

    Seccion 02

---
+++ S02_Variables_Operadores/04_Tipos_Datos/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String [] args){
+        byte edad = 25;
+        System.out.println(edad);
+
+        long numerosGrandes = 94581267L;
+        System.out.println(numerosGrandes);
+
+        float nd = 3.5f;
+        System.out.println(nd);
+
+        double ndd = 3.55;
+        System.out.println(ndd);
+
+        char letra = 'a';
+        char codigo = 66;
+        System.out.println(letra);
+        System.out.println(codigo);
+
+        boolean opcion = true;
+        opcion = false;
+        System.out.println(opcion);
+
+        String nombreCompleto = "Miguel Alexander";
+        System.out.println(nombreCompleto);
+
+
+
+    }
+}
