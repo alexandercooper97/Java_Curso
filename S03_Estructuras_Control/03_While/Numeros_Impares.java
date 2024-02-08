dc56792eead00d2b2d081701542d8421edfd428a
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-07 22:45:17 -0500

    Seccion 03

---
+++ S03_Estructuras_Control/03_While/Numeros_Impares.java
@@ -0,0 +1,11 @@
+
+public class Numeros_Impares {
+    public static void main(String[] args) {
+        int j = 2;
+        while (j <= 20) {
+            System.out.println(j);
+            j += 2;
+        }
+
+    }
+}
