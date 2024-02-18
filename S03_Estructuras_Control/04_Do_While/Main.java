6dce16a980bc312fc2f957f6783583b55e32ad04
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-17 22:44:53 -0500

    Seccion 03

---
+++ S03_Estructuras_Control/04_Do_While/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int c = 0;
+        do{
+            System.out.println("El valor de c es : " + c);
+            c++;
+        }while( c <= 10);
+    }
+}
