c19f5c1deeebb074e013b1076af0b700d7a24b6f
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-17 22:50:45 -0500

    Seccion 03

---
+++ S03_Estructuras_Control/05_Bucle_For/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 8;
+
+        for(int i=10; i<=20; i++){
+            System.out.printf("%d x %d = %d\n", n, i,(n*i));
+        }
+    }
+    
+}
