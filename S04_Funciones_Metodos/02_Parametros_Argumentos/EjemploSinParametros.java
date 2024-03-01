d8e04931195fedced6511e72640eb367c68317bb
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-02-29 23:11:17 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/02_Parametros_Argumentos/EjemploSinParametros.java
@@ -0,0 +1,9 @@
+public class EjemploSinParametros {
+    public static void main(String[] args) {
+        imprimirSaludo();
+    }
+
+    public static void imprimirSaludo() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
