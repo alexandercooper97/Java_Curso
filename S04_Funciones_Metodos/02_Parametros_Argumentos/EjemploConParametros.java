e603d47f19bf971cc3d865d444332570cdfa3770
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-01 23:40:20 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/02_Parametros_Argumentos/EjemploConParametros.java
@@ -0,0 +1,10 @@
+public class EjemploConParametros {
+    public static void main(String[] args) {
+        imprimirSaludo("¡Hola, mundo!");
+        imprimirSaludo("¡Hola, Juan!");
+    }
+
+    public static void imprimirSaludo(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
