762587c0f1deccb583ca35e0d740e134179d551e
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-17 22:56:35 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/05_Recursividad/RecursividadJava.java
@@ -0,0 +1,17 @@
+public class RecursividadJava {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println("El factorial de " + numero + " es: " + factorial(numero));
+    }
+
+    public static int factorial(int n) {
+        // Caso base
+        if (n == 0) {
+            return 1;
+        }
+        // Caso recursivo
+        else {
+            return n * factorial(n - 1);
+        }
+    }
+}
