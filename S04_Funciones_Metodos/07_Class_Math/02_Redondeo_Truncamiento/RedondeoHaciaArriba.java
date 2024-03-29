096988e9834fed21732561017f6d4003c09af76b
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-28 22:51:02 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/02_Redondeo_Truncamiento/RedondeoHaciaArriba.java
@@ -0,0 +1,7 @@
+public class RedondeoHaciaArriba {
+    public static void main(String[] args) {
+        double numero = 5.3;
+        double resultado = Math.ceil(numero);
+        System.out.println("Redondeando hacia arriba el número " + numero + ": " + resultado);
+    }
+}
