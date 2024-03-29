c6802971a39606b58e260a8ac9d2506d221fcb7d
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-28 22:53:33 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/02_Redondeo_Truncamiento/RedondeoCercano.java
@@ -0,0 +1,7 @@
+public class RedondeoCercano {
+    public static void main(String[] args) {
+        double numero = 5.5;
+        long resultado = Math.round(numero);
+        System.out.println("Redondeando al entero más cercano el número " + numero + ": " + resultado);
+    }
+}
