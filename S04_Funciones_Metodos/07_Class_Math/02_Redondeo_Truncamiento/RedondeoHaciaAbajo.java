b53d5657eac7ecf997590b327f85cd9ce16c6f73
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-28 22:52:32 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/02_Redondeo_Truncamiento/RedondeoHaciaAbajo.java
@@ -0,0 +1,7 @@
+public class RedondeoHaciaAbajo {
+    public static void main(String[] args) {
+        double numero = 5.7;
+        double resultado = Math.floor(numero);
+        System.out.println("Redondeando hacia abajo el número " + numero + ": " + resultado);
+    }
+}
