9a5a456e4f570aaeef9ee96282a4d03d569db3a6
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-29 21:19:47 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/03_Potencia_Radicacion/RaizCuadrada.java
@@ -0,0 +1,7 @@
+public class RaizCuadrada {
+    public static void main(String[] args) {
+        double numero = 25;
+        double resultado = Math.sqrt(numero);
+        System.out.println("La raíz cuadrada del número " + numero + " es: " + resultado);
+    }
+}
