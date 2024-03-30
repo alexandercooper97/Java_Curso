ca98c15c55faf645aa4c28f84c72c16bcf24f110
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-29 21:20:04 -0500

    Delete S04_Funciones_Metodos/07_Class_Math/RaizCuadrada.java

--- S04_Funciones_Metodos/07_Class_Math/RaizCuadrada.java
+++
@@ -1,7 +0,0 @@
-public class RaizCuadrada {
-    public static void main(String[] args) {
-        double numero = 25;
-        double resultado = Math.sqrt(numero);
-        System.out.println("La raíz cuadrada del número " + numero + " es: " + resultado);
-    }
-}
