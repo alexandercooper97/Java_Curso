66e2443d3d79a2296a67c012d074773aa543157f
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-29 21:20:23 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/03_Potencia_Radicacion/RaizCubica.java
@@ -0,0 +1,7 @@
+public class RaizCubica {
+    public static void main(String[] args) {
+        double numero = 27;
+        double resultado = Math.cbrt(numero);
+        System.out.println("La raíz cúbica del número " + numero + " es: " + resultado);
+    }
+}
