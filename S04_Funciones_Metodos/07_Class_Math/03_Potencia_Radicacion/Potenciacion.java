439bd6d8e2a6c2506b12bb7aabe1c7060f90bc66
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-29 21:17:53 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/03_Potencia_Radicacion/Potenciacion.java
@@ -0,0 +1,8 @@
+public class Potenciacion {
+    public static void main(String[] args) {
+        double base = 5;
+        double exponente = 3;
+        double resultado = Math.pow(base, exponente);
+        System.out.println("El número " + base + " elevado a la potencia " + exponente + " es: " + resultado);
+    }
+}
