d7cae414f7aebdaf6d2287df333d2b60abf68d6c
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-29 21:24:53 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/05_Numeros_Aleatorios/Aleatorio.java
@@ -0,0 +1,6 @@
+public class Aleatorio {
+    public static void main(String[] args) {
+        double aleatorio = Math.random();
+        System.out.println("Número aleatorio entre 0 y 1: " + aleatorio);
+    }
+}
