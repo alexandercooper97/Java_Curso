15bb2d11f3c438bce9d74971d8521d55ea260732
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 22:54:11 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/07_Conversion_Operaciones_Angulos/GradosARadianes.java
@@ -0,0 +1,7 @@
+public class GradosARadianes {
+    public static void main(String[] args) {
+        double anguloGrados = 45;
+        double anguloRadianes = Math.toRadians(anguloGrados);
+        System.out.println(anguloGrados + " grados equivalen a: " + anguloRadianes + " radianes");
+    }
+}
