96cbc356ae49c2deeb06ccc63182c2e30128f740
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 22:50:43 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/06_Comparacion_Seleccion_Valores/Maximo.java
@@ -0,0 +1,8 @@
+public class Maximo {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 20;
+        int maximo = Math.max(num1, num2);
+        System.out.println("El mayor entre " + num1 + " y " + num2 + " es: " + maximo);
+    }
+}
