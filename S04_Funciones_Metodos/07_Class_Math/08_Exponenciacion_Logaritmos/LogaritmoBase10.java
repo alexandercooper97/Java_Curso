598b7c67bda4758f6875a4db2b42ca84d4557ed1
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 23:02:47 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/08_Exponenciacion_Logaritmos/LogaritmoBase10.java
@@ -0,0 +1,7 @@
+public class LogaritmoBase10 {
+    public static void main(String[] args) {
+        double num = 100.0;
+        double logaritmoBase10 = Math.log10(num);
+        System.out.println("El logaritmo en base 10 de " + num + " es: " + logaritmoBase10);
+    }
+}
