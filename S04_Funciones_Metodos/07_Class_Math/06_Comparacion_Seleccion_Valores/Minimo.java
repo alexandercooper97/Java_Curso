b769fe372bafb901c68c179b4239af1d49da01c2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 22:49:56 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/06_Comparacion_Seleccion_Valores/Minimo.java
@@ -0,0 +1,8 @@
+public class Minimo {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int minimo = Math.min(num1, num2);
+        System.out.println("El menor entre " + num1 + " y " + num2 + " es: " + minimo);
+    }
+}
