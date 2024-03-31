3416cdf42ca56a24e3c4f27b7dc16230ec46a8ba
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 22:59:12 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/08_Exponenciacion_Logaritmos/Exponenciacion.java
@@ -0,0 +1,7 @@
+public class Exponenciacion {
+    public static void main(String[] args) {
+        double num = 2.0;
+        double resultado = Math.exp(num);
+        System.out.println("e elevado a la potencia de " + num + " es: " + resultado);
+    }
+}
