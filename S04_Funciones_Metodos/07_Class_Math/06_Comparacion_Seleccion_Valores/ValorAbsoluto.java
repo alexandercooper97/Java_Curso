19c0b6ed8e746225108d8528b41b41444b672416
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 22:51:33 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/06_Comparacion_Seleccion_Valores/ValorAbsoluto.java
@@ -0,0 +1,7 @@
+public class ValorAbsoluto {
+    public static void main(String[] args) {
+        int num = -10;
+        int absoluto = Math.abs(num);
+        System.out.println("El valor absoluto de " + num + " es: " + absoluto);
+    }
+}
