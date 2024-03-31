cd9b08a978a629d9729c456bb440f3634f5fc29a
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2024-03-30 23:01:36 -0500

    Seccion 04

---
+++ S04_Funciones_Metodos/07_Class_Math/08_Exponenciacion_Logaritmos/LogaritmoNatural.java
@@ -0,0 +1,7 @@
+public class LogaritmoNatural {
+    public static void main(String[] args) {
+        double num = 7.0;
+        double logaritmoNatural = Math.log(num);
+        System.out.println("El logaritmo natural de " + num + " es: " + logaritmoNatural);
+    }
+}
